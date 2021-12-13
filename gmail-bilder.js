cb6aa217421cd86be714fa36d376e1551acc446a
Lukas Dattenberger <lukas.datte@gmail.com>
2021-12-13 10:26:54 +0100

    Add gmail-bilder.js

---
+++ gmail-bilder.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Gmail CSS (Bilder)
+// @namespace    https://github.com/lukasdatte/HusqWebOrder
+// @version      1.0.0
+// @description  Stellt sicher, dass die Bilder in Gmail nicht breiter als das Fenster werden.
+// @author       Lukas Dattenberger
+// @match        https://mail.google.com/mail/u/0/*
+// @icon         https://www.google.com/s2/favicons?domain=google.com
+// @updateURL    https://raw.githubusercontent.com/lukasdatte/HusqWebOrder/main/gmail-bilder.js
+// @downloadURL  https://raw.githubusercontent.com/lukasdatte/HusqWebOrder/main/gmail-bilder.js
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    GM_addStyle('div[data-message-id] img {max-width: 100%;}');
+    console.log('gmail css tempered with - gmail-bilder.js');
+})();
